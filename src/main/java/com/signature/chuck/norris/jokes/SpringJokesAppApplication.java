1f130f84d8b37cff397595a0806bd6ca42e73a46
jarvis <atul.atendra15@gmail.com>
2021-08-16 14:49:18 +0530

    added SFG dependency for jokes

---
+++ src/main/java/com/signature/chuck/norris/jokes/SpringJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.signature.chuck.norris.jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJokesAppApplication.class, args);
+	}
+
+}
