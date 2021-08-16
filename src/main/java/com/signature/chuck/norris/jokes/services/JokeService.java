37e1b3f989b66369e310d229eb5c68769397e39d
jarvis <atul.atendra15@gmail.com>
2021-08-16 14:53:39 +0530

    adding joke service

---
+++ src/main/java/com/signature/chuck/norris/jokes/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.signature.chuck.norris.jokes.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/com/signature/chuck/norris/jokes/services/JokeServiceImpl.java
@@ -0,0 +1,18 @@
+package com.signature.chuck.norris.jokes.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return this.chuckNorrisQuotes.getRandomQuote();
+    }
+
+}
